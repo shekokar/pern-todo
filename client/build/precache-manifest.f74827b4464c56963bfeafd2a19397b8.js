47985dee502856a6e2b29fdb7efb29493192dabb
Sarang Shekokar <shekokarsarang@gmail.com>
2020-03-20 11:49:54 +0530

    final commit

---
+++ client/build/precache-manifest.f74827b4464c56963bfeafd2a19397b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "690b499de81a7de0adcdce282cef4ba6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcc3b6e1a84796417c2a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8354ae98b801edde7f40",
+    "url": "/static/js/2.5459a816.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.5459a816.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcc3b6e1a84796417c2a",
+    "url": "/static/js/main.d1908a99.chunk.js"
+  },
+  {
+    "revision": "7c5c58706d295695379f",
+    "url": "/static/js/runtime-main.1fdb64ba.js"
+  }
+]);
